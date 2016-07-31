51685b6e428a27839d520e96c6dfff897695b5d5
lowagner <lowagner@gmail.com>
2016-07-31 07:56:44 -0500

    extra checking for bug finding

--- main.c
+++ main.c
@@ -5,7 +5,11 @@
 #include <string.h>
 
 uint16_t color CCM_MEMORY;
+uint16_t old_color CCM_MEMORY;
+uint8_t stripe CCM_MEMORY;
+uint8_t update_stripe CCM_MEMORY;
 uint8_t music_on CCM_MEMORY;
+uint32_t press_hold CCM_MEMORY;
 
 #include "fatfs/ff.h"
 
@@ -14,9 +18,10 @@ uint8_t music_on CCM_MEMORY;
 FATFS fat_fs CCM_MEMORY;
 FIL fat_file CCM_MEMORY;
 uint8_t mount_ok CCM_MEMORY;
-int file_count;
-int file_index;
-#define MAX_FILES 50
+uint8_t load_next_file_please CCM_MEMORY;
+int file_count CCM_MEMORY;
+int file_index CCM_MEMORY;
+#define MAX_FILES 64
 char filenames[MAX_FILES][13];
 
 #ifdef EMULATOR
@@ -25,6 +30,63 @@ char filenames[MAX_FILES][13];
 #define ROOT_DIR ""
 #endif
 
+void get_file_index()
+{
+    if (!mount_ok)
+    {
+        color = RGB(255,255,255);
+        update_stripe = 1;
+        return;
+    }
+    UINT bytes_get;
+    if (f_open(&fat_file, "SONGNOSE.TXT", FA_READ | FA_OPEN_EXISTING) != FR_OK)
+    {
+        file_index = 0;
+        message("error opening SONGNOSE.TXT file\n");
+        color = RGB(255,150,0);
+        update_stripe = 1;
+        return;
+    }
+    if (f_read(&fat_file, &file_index, sizeof(file_index), &bytes_get) != FR_OK ||
+        bytes_get != sizeof(file_index))
+    {
+        message("error reading SONGNOSE.TXT file\n");
+        color = RGB(255,0,0);
+        update_stripe = 1;
+        file_index = 0;
+    }
+    message("got file_index %d\n", file_index);
+    f_close(&fat_file);
+}
+
+void set_file_index(int skip)
+{
+    if (!mount_ok)
+    {
+        color = RGB(255,255,255);
+        update_stripe = 1;
+        return;
+    }
+    UINT bytes_get;
+    
+    if (f_open(&fat_file, "SONGNOSE.TXT", FA_WRITE | FA_CREATE_ALWAYS) != FR_OK) 
+    {
+        message("error opening SONGNOSE.TXT file to write\n");
+        color = RGB(255,0,255);
+        update_stripe = 1;
+        return;
+    }
+    if (f_write(&fat_file, &skip, sizeof(skip), &bytes_get) != FR_OK ||
+        bytes_get != sizeof(skip))
+    {
+        color = RGB(100,0,255);
+        update_stripe = 1;
+        message("error in writing file_index to SONGNOSE.TXT\n");
+    }
+    f_close(&fat_file);
+}
+
+
 static int cmp(const void *p1, const void *p2){
     return strcmp( (char * const ) p1, (char * const ) p2);
 }
@@ -32,7 +94,7 @@ static int cmp(const void *p1, const void *p2){
 void list_music() // adapted from list_roms from bitbox/2nd_boot
 {
     message("listing music\n");
-    file_index = 0;
+    get_file_index();
 
     FRESULT res;
     FILINFO fno;
@@ -43,25 +105,58 @@ void list_music() // adapted from list_roms from bitbox/2nd_boot
     if (f_opendir(&dir, ROOT_DIR) != FR_OK) 
     {
         color = RGB(255, 255, 0);
+        update_stripe = 1;
         message("bad dir\n");
         return;
     }
-    for (file_count=0; file_count<MAX_FILES;) 
+    int looped = 0;
+    for (file_count=0;;) 
     {
         res = f_readdir(&dir, &fno);                   /* Read a directory item */
+        message("checking file %s\n", fno.fname);
         if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
-        if (fno.fname[0] == '.' || (fno.fattrib & AM_DIR)) /* Ignore dot entry and dirs */
+        /* Ignore dot entry and dirs */
+        if (fno.fname[0] == '.')
+        {
+            message("this was a dot\n");
             continue;
+        }
+        if (fno.fattrib & AM_DIR) 
+        {
+            message("this was a directory\n");
+            continue;
+        }
         fn = fno.fname;
-        message("checking file %s\n", fn);
         // check extension : only keep .w8v
         if (strstr(fn,".W8V") || strstr(fn,".w8v")) // search ignoring case
-            strcpy(filenames[file_count++],fn);
+        {
+            message("got w8v\n");
+            int buffer_index = file_count % MAX_FILES;
+            if (buffer_index == file_index % MAX_FILES)
+            {
+                if (looped == 1 + file_index/MAX_FILES)
+                    break;
+                else
+                    ++looped;
+            }
+            strcpy(filenames[buffer_index],fn);
+            ++file_count;
+        }
     }
     f_closedir(&dir);
+    message("got to end with count %d\n", file_count);
+    // check if we looped more than once:
+    if (file_count > MAX_FILES)
+    {
+        message(" we were aiming at index [%d, +64)\n", file_index);
+        file_count = MAX_FILES;
+    }
+    set_file_index(file_index + file_count/2);
+    if (!file_count)
+        return;
+    file_index %= file_count;
 
     // sort it
-    message("got to end with count %d\n", file_count);
     qsort(filenames, file_count, 13, cmp);
     for (int i=0; i<file_count; ++i)
     {
@@ -71,13 +166,24 @@ void list_music() // adapted from list_roms from bitbox/2nd_boot
 
 void load_next_file()
 {
+    load_next_file_please = 0;
+
     music_on = 0;
     if (!mount_ok)
+    {
+        color = RGB(255,255,255);
         return;
+    }
+    if (!file_count)
+    {
+        color = RGB(255,255,0);
+        return;
+    }
     
     if (file_index >= file_count)
         file_index = 0;
-    message("opening the %d .w8v: ", file_index);
+    color = RGB(0,255,255);
+    message("opening the %d music, %s.w8v: ", file_index, filenames[file_index]);
     if (f_open(&fat_file, filenames[file_index++], FA_READ | FA_OPEN_EXISTING) != FR_OK)
     {
         message("NO OPEN!\n");
@@ -85,12 +191,15 @@ void load_next_file()
         music_on = 0;
         return;
     }
+    color = RGB(0,255,0);
     message("ok!\n");
     music_on = 1;
 }
 
 void game_init()
 {
+    stripe = 0;
+    old_color = 0;
     color = 0;
     mount_ok = 0;
     music_on = 0;
@@ -98,6 +207,7 @@ void game_init()
     if (f_mount(&fat_fs, "", 1) != FR_OK)
     {
         color = RGB(255,255,255);
+        update_stripe = 1;
         return;
     }
     mount_ok = 255;
@@ -106,6 +216,8 @@ void game_init()
 
 void game_frame()
 {
+    if (load_next_file_please)
+        load_next_file();
 }
 
 #ifndef NO_VGA
@@ -115,9 +227,33 @@ void graph_frame()
 
 void graph_line() 
 {
+    if (vga_odd)
+        return;
     //const uint8_t max_color = 8*(music_on+1);
     //color = ((rand()%max_color)<<10)|((rand()%max_color)<<5)|(rand()%max_color);
-    draw_buffer[rand()%320] = color;
+    if (update_stripe)
+    {
+        if (vga_line/2 == 0)
+        {
+            draw_buffer[stripe*10] = old_color;
+            draw_buffer[stripe*10+1] = old_color;
+            draw_buffer[stripe*10+8] = old_color;
+            draw_buffer[stripe*10+9] = old_color;
+        }
+        else if (vga_line == 240-1)
+        {
+            stripe = (stripe+1)%32;
+            update_stripe = 0;
+            old_color = color;
+        }
+        return;
+    }
+    draw_buffer[stripe*10] = RGB(255,255,255);
+    draw_buffer[stripe*10+1] = RGB(255,255,255);
+    if ((vga_line/2)%16 == 0 && vga_frame % 16 == 0)
+        draw_buffer[stripe*10 + 2 + rand()%6] = color;
+    draw_buffer[stripe*10+8] = RGB(255,255,255);
+    draw_buffer[stripe*10+9] = RGB(255,255,255);
 }
 #endif
 
@@ -151,19 +287,24 @@ static inline uint16_t gen_sample()
 void game_snd_buffer(uint16_t *buffer, int len) 
 {
     static uint16_t press=0;
-    static uint32_t press_hold=0;
     if (press)
     {
         press = button_state();
         if (!press)
         {
             // button release, do something fancier for holding down the button
-            music_on = (music_on+1)%2;
-            if (music_on == 1)
+            if (music_on)
             {
-                color = RGB(0,255,255);
-                load_next_file();
+                music_on = 0;
+                message("pre-empted\n");
+                color = RGB(255,150,100);
+                f_close(&fat_file);
             }
+            else
+            {
+                load_next_file_please = 1;
+            }
+            press_hold = 0;
         }
         else
         {
@@ -171,16 +312,23 @@ void game_snd_buffer(uint16_t *buffer, int len)
             ++press_hold;
             const uint8_t max_color = 8*(music_on+1);
             color = ((rand()%max_color)<<10)|((rand()%max_color)<<5)|(rand()%max_color);
-           
+        }
+        memset(buffer, 128, 2*len);
+        return;
+    }
+    else
+    {
+        press = button_state();
+        if (press)
+        {
+            message("got press %d?\n", press);
+            old_color = color;
+            update_stripe = 1;
             memset(buffer, 128, 2*len);
             return;
         }
     }
-    else
-    {
-        press_hold = 0;
-        press = button_state();
-    }
+
     if (!music_on)
     {
         for (int i=0; i<len; i++)
