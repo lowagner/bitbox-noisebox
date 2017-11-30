47c4c986d9e46b619d1a798059faaca0e789cd38
Lucas Wagner <lowagner@gmail.com>
2017-11-29 21:51:07 -0800

    new submodule kernel sdk approach

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
-# DO NOT FORGET to define BITBOX environment variable 
 
-#NO_VGA = 1 # comment out to allow graphics, do a "make clean" to reset.
-USE_SDCARD = 1 # comment out to get no wav files
+VGA_MODE=NONE
+#VGA_MODE=320 
+#USE_SDCARD = 1 # comment out to get no wav files
+NO_USB=1
 
 NAME = noisebox
 GAME_C_FILES = main.c
 GAME_H_FILES =
 
-ifndef NO_VGA
+ifneq ($(VGA_MODE), NONE)
 GAME_C_FILES += font.c
 GAME_H_FILES += font.h
 endif
 
-GAME_C_OPTS = -DVGAMODE_320 -DBITBOX_SAMPLERATE=16000 -DBITBOX_SNDBUF_LEN=1066
+GAME_C_OPTS = -DVGA_MODE=$(VGA_MODE) -DBITBOX_SAMPLERATE=16000 -DBITBOX_SNDBUF_LEN=1066
 
 # see this file for options
-include $(BITBOX)/lib/bitbox.mk
+BITBOX=bitbox
+include $(BITBOX)/kernel/bitbox.mk
--- main.c
+++ main.c
@@ -5,13 +5,45 @@
 #include <stdio.h>
 #include <string.h>
 
+static inline uint16_t gen_sample()
+{
+    // This is a simple noise generator based on an LFSR (linear feedback shift
+    // register). It is fast and simple and works reasonably well for audio.
+    static uint32_t noiseseed[2] = {1, 150};
+    static uint32_t rednoise[2] = {0, 3};
+    for (int i=0; i<2; ++i)
+    {
+        uint32_t newbit = 0;
+        if (noiseseed[i] & 0x80000000L) newbit ^= 1;
+        if (noiseseed[i] & 0x01000000L) newbit ^= 1;
+        if (noiseseed[i] & 0x00000040L) newbit ^= 1;
+        if (noiseseed[i] & 0x00000200L) newbit ^= 1;
+        noiseseed[i] = (noiseseed[i] << 1) | newbit;
+        rednoise[i] = 3*rednoise[i]/4 + (noiseseed[i]&255)/4;
+    }
+
+    // Now put the two channels together in the output word
+    return (128-32+(rednoise[0]&255)/4) | ((128-32+(rednoise[1]&255)/4)<<8);
+}
+
+#ifndef USE_SDCARD
+void game_init()
+{
+}
+
+void game_snd_buffer(uint16_t *buffer, int len) 
+{
+    for (int i=0; i<len; i++)
+        buffer[i] = gen_sample();
+}
+#else
 #define MAX_FILES 4096 
 #define PLAY_TIME (45*957021ul) // approx 45 minutes.  choose whatever you like!
 
 uint64_t time_passed CCM_MEMORY;
 uint64_t song_pos CCM_MEMORY;
 uint8_t last_song CCM_MEMORY;
-#ifndef NO_VGA
+#if VGA_MODE != NONE
 uint16_t color_ring[32] CCM_MEMORY;
 uint8_t color_ring_end CCM_MEMORY;
 uint8_t message_ring[16][32] CCM_MEMORY;
@@ -33,6 +65,7 @@ static inline void push_message(const char *c)
     message_ring[message_ring_end][31] = 0; // ensure null termination
 }
 #endif
+
 
 uint8_t music_on CCM_MEMORY;
 uint32_t press_hold CCM_MEMORY;
@@ -59,7 +92,7 @@ void get_file_index()
 {
     if (!mount_ok)
     {
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_color(RGB(255,255,255));
         #endif
         return;
@@ -68,7 +101,7 @@ void get_file_index()
     if (f_open(&fat_file, "SONGNOSE.TXT", FA_READ | FA_OPEN_EXISTING) != FR_OK)
     {
         file_index = 0;
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_message("error opening SONGNOSE.TXT");
         push_color(RGB(255,150,0));
         #endif
@@ -78,12 +111,12 @@ void get_file_index()
         bytes_get != sizeof(file_index))
     {
         file_index = 0;
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_message("error reading SONGNOSE.TXT");
         push_color(RGB(255,0,0));
         #endif
     }
-    #ifndef NO_VGA
+    #if VGA_MODE != NONE
     int digits = file_index;
     push_message(" file_index: ");
     message_ring[message_ring_end][22] = 0;
@@ -100,7 +133,7 @@ void set_file_index(int skip)
 {
     if (!mount_ok)
     {
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_color(RGB(255,255,255));
         #endif
         return;
@@ -110,7 +143,7 @@ void set_file_index(int skip)
     if (f_open(&fat_file, "SONGNOSE.TXT", FA_WRITE | FA_CREATE_ALWAYS) != FR_OK) 
     {
         message("error opening SONGNOSE.TXT file to write\n");
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_color(RGB(255,0,255));
         #endif
         return;
@@ -118,7 +151,7 @@ void set_file_index(int skip)
     if (f_write(&fat_file, &skip, sizeof(skip), &bytes_get) != FR_OK ||
         bytes_get != sizeof(skip))
     {
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_color(RGB(100,0,255));
         #endif
         message("error in writing file_index to SONGNOSE.TXT\n");
@@ -143,7 +176,7 @@ void list_music() // adapted from list_roms from bitbox/2nd_boot
 
     if (f_opendir(&dir, ROOT_DIR) != FR_OK) 
     {
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_color(RGB(255, 255, 0));
         push_message("bad dir\n");
         #endif
@@ -156,13 +189,13 @@ void list_music() // adapted from list_roms from bitbox/2nd_boot
         if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
         /* Ignore dot entry and dirs */
         if (fn[0] == '.') continue;
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_message("check: ");
         strcpy((char *)&message_ring[message_ring_end][7], fn);
         #endif
         if (fno.fattrib & AM_DIR) 
         {
-            #ifndef NO_VGA
+            #if VGA_MODE != NONE
             push_message(" was a directory\n");
             #endif
             continue;
@@ -170,7 +203,7 @@ void list_music() // adapted from list_roms from bitbox/2nd_boot
         // check extension : only keep .w8v
         if (strstr(fn,".W8V") || strstr(fn,".w8v")) // search ignoring case
         {
-            #ifndef NO_VGA
+            #if VGA_MODE != NONE
             push_message(" got w8v.");
             #endif
             strcpy(filenames[file_count++],fn);
@@ -191,7 +224,7 @@ void list_music() // adapted from list_roms from bitbox/2nd_boot
 
 void load_next_file()
 {
-    #ifndef NO_VGA
+    #if VGA_MODE != NONE
     push_color(RGB(100,100,100));
     push_message("requesting next file");
     #endif
@@ -200,7 +233,7 @@ void load_next_file()
     music_on = 0;
     if (!mount_ok)
     {
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_color(RGB(255,255,255));
         push_message("no mount");
         #endif
@@ -208,7 +241,7 @@ void load_next_file()
     }
     if (!file_count)
     {
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_color(RGB(255,255,0));
         push_message("no files");
         #endif
@@ -219,7 +252,7 @@ void load_next_file()
     {
         time_passed = 0;
         // stop after reaching end of playlist
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_color(RGB(255,0,0));
         #endif
         // move the file index ahead
@@ -232,20 +265,20 @@ void load_next_file()
     if (file_index >= file_count)
         file_index = 0;
     message("opening the %d music, %s: ", file_index, filenames[file_index]);
-    #ifndef NO_VGA
+    #if VGA_MODE != NONE
     push_color(RGB(0,255,255));
     push_message("file: ");
     strcpy((char *)&message_ring[message_ring_end][6], filenames[file_index]);
     #endif
     if (f_open(&fat_file, filenames[file_index++], FA_READ | FA_OPEN_EXISTING) != FR_OK)
     {
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_message(" NO OPEN!\n");
         push_color(RGB(255,0,255));
         #endif
         return;
     }
-    #ifndef NO_VGA
+    #if VGA_MODE != NONE
     push_color(RGB(0,255,0));
     push_message(" ok!");
     #endif
@@ -256,7 +289,7 @@ void game_init()
 {
     time_passed = 0;
     last_song = 0;
-    #ifndef NO_VGA
+    #if VGA_MODE != NONE
     font_init();
     message_ring_end = 15;
     stripe = 0;
@@ -266,74 +299,18 @@ void game_init()
         
     if (f_mount(&fat_fs, "", 1) != FR_OK)
     {
-        #ifndef NO_VGA
+        #if VGA_MODE != NONE
         push_color(RGB(255,255,255));
         push_message("mount failure");
         #endif
         return;
     }
-    #ifndef NO_VGA
+    #if VGA_MODE != NONE
     push_color(RGB(100,100,100));
     push_message("mount ok");
     #endif
     mount_ok = 255;
     list_music();
-}
-
-void game_frame()
-{
-}
-
-#ifndef NO_VGA
-void graph_frame() 
-{
-}
-
-void graph_line() 
-{
-    static int line_length = 0;
-    if (vga_odd)
-        return;
-    uint16_t color = color_ring[color_ring_end];
-    uint16_t stripe = 10*color_ring_end;
-    draw_buffer[stripe] = RGB(255,255,255);
-    if ((vga_line/2)%16 == 0 && vga_frame % 16 == 0)
-        draw_buffer[stripe + 1 + rand()%8] = color;
-    draw_buffer[stripe+9] = RGB(255,255,255);
-
-    if (vga_line < 48 || vga_line >= 48+16*10)
-        return;
-    int line = (vga_line-48);
-    int internal_line = line%10;
-    line /= 10;
-    if (internal_line < 8)
-        line_length = font_render_line_doubled(message_ring[line], 16, internal_line, 
-            line == message_ring_end ? RGB(255,0,0) : 65535, 
-        0);
-    else
-        memset(&draw_buffer[16], 0, line_length*10*2);
-}
-#endif
-
-static inline uint16_t gen_sample()
-{
-    // This is a simple noise generator based on an LFSR (linear feedback shift
-    // register). It is fast and simple and works reasonably well for audio.
-    static uint32_t noiseseed[2] = {1, 150};
-    static uint32_t rednoise[2] = {0, 3};
-    for (int i=0; i<2; ++i)
-    {
-        uint32_t newbit = 0;
-        if (noiseseed[i] & 0x80000000L) newbit ^= 1;
-        if (noiseseed[i] & 0x01000000L) newbit ^= 1;
-        if (noiseseed[i] & 0x00000040L) newbit ^= 1;
-        if (noiseseed[i] & 0x00000200L) newbit ^= 1;
-        noiseseed[i] = (noiseseed[i] << 1) | newbit;
-        rednoise[i] = 3*rednoise[i]/4 + (noiseseed[i]&255)/4;
-    }
-
-    // Now put the two channels together in the output word
-    return (128-32+(rednoise[0]&255)/4) | ((128-32+(rednoise[1]&255)/4)<<8);
 }
 
 void game_snd_buffer(uint16_t *buffer, int len) 
@@ -353,7 +330,7 @@ void game_snd_buffer(uint16_t *buffer, int len)
                 {
                     music_on = 0;
                     message("pre-empted\n");
-                    #ifndef NO_VGA
+                    #if VGA_MODE != NONE
                     push_color(RGB(255,150,100));
                     #endif
                     f_close(&fat_file);
@@ -361,7 +338,7 @@ void game_snd_buffer(uint16_t *buffer, int len)
                 }
                 else
                 {
-                    #ifndef NO_VGA
+                    #if VGA_MODE != NONE
                     push_color(RGB(200,100,0));
                     #endif
                     seek_forward = 512*(press_hold + press_hold*press_hold);
@@ -377,7 +354,7 @@ void game_snd_buffer(uint16_t *buffer, int len)
         {
             // still pressing the button
             ++press_hold;
-            #ifndef NO_VGA
+            #if VGA_MODE != NONE
             const uint8_t max_color = 8*(music_on+1);
             color_ring[(color_ring_end-1)&255] =  ((rand()%max_color)<<10)|((rand()%max_color)<<5)|(rand()%max_color);
             #endif
@@ -391,7 +368,7 @@ void game_snd_buffer(uint16_t *buffer, int len)
         if (press)
         {
             message("got press %d?\n", press);
-            #ifndef NO_VGA
+            #if VGA_MODE != NONE
             ++color_ring_end;
             #endif
             memset(buffer, 128, 2*len);
@@ -430,7 +407,7 @@ void game_snd_buffer(uint16_t *buffer, int len)
         UINT bytes_read;
         if (f_read(&fat_file, buffer, bytes_to_read, &bytes_read) != FR_OK)
         {
-            #ifndef NO_VGA
+            #if VGA_MODE != NONE
             push_message(" file not read well");
             push_color(RGB(255,0,255));
             #endif
@@ -440,7 +417,7 @@ void game_snd_buffer(uint16_t *buffer, int len)
         }
         else if (bytes_read != bytes_to_read)
         {
-            #ifndef NO_VGA
+            #if VGA_MODE != NONE
             push_message(" file ended.");
             push_color(RGB(255,0,0));
             #endif
@@ -458,3 +435,39 @@ void game_snd_buffer(uint16_t *buffer, int len)
     time_passed += len/2;
     return;
 }
+#endif // USE_SDCARD
+
+void game_frame()
+{
+}
+
+#if VGA_MODE != NONE
+void graph_line() 
+{
+    static int line_length = 0;
+    if (vga_odd)
+        return;
+    uint16_t color = color_ring[color_ring_end];
+    uint16_t stripe = 10*color_ring_end;
+    draw_buffer[stripe] = RGB(255,255,255);
+    if ((vga_line/2)%16 == 0 && vga_frame % 16 == 0)
+        draw_buffer[stripe + 1 + rand()%8] = color;
+    draw_buffer[stripe+9] = RGB(255,255,255);
+
+    if (vga_line < 48 || vga_line >= 48+16*10)
+        return;
+    int line = (vga_line-48);
+    int internal_line = line%10;
+    line /= 10;
+    if (internal_line < 8)
+        line_length = font_render_line_doubled(message_ring[line], 16, internal_line, 
+            line == message_ring_end ? RGB(255,0,0) : 65535, 
+        0);
+    else
+        memset(&draw_buffer[16], 0, line_length*10*2);
+}
+#else
+void graph_line()
+{
+}
+#endif
